15c98f65623b6202cec6edff95758869bb614dc1
druetta_nicolas <nsdruetta@gmail.com>
2022-12-02 16:47:36 -0300

    error al cargar la pagina

--- my-app/src/components/navbar/categorias/categorias.jsx
+++ my-app/src/components/navbar/categorias/categorias.jsx
@@ -1,7 +1,6 @@
 const Categorias = () => {
     return (
         <>
-        <Categorias/> 
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">Instrumentos</a>
